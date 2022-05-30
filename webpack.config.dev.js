84efd7637e06a9a038b3b03b9d166bfe7be770c5
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-05-30 17:16:10 -0500

    Optimización de webpack para React

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        clean: true,
-        publicPath: "/",
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles': path.resolve(__dirname, 'src/styles/')
-        }
+        extensions: ['.js', '.jsx']
     },
-    mode: 'production',
+    mode: 'development',
     module: {
         rules: [
             {
@@ -54,11 +46,12 @@ module.exports = {
             filename: '[name].css'
         })
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+            },
+            compress: true,
+            port: 3006,
+            open: true,
     }
 }
